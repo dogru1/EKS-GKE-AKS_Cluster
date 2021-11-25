4ce6d3a5ce0b0ffb0675b8c52addc80519b61434
DJ <88846807+dogru1@users.noreply.github.com>
2021-11-24 19:28:02 -0600

    AKS Cluster

---
+++ AKS/ad_group.tf
@@ -0,0 +1,11 @@
+resource "azuread_group" "example" {
+	display_name = "DevOps"
+	mail_enabled = true
+	mail_nickname = "ExampleGroup"
+	security_enabled = true
+	types = ["Unified"]
+	# members = [
+	# azuread_user.example.object_id,
+     # /* more users */
+	# ]
+}
